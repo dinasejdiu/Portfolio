e79e9dd310e0414aedb7c1a391c29260ef5bd478
Erodina <es49107@ubt-uni.net>
2022-01-29 01:05:10 +0100

    Modeli i dergimit te mesazhit dhe historija e kyqjes

--- Portfolio/Models/Message.cs
+++ Portfolio/Models/Message.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Message
+    {
+        [Required(ErrorMessage = "Ju lutem shkruani emrin.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Emri")]
+        public string Emri { get; set; }
+        [Required(ErrorMessage = "Ju lutem shkruani mbiemrin.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Mbiemri:")]
+        public string Mbiemri { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Gender:")]
+        public string Gjinia { get; set; }
+        [Required(ErrorMessage = "Shkruani email tuaj.")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Adresa:")]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Numrin e telefonit nese mundeni:")]
+        public string Telefoni { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Komentet:")]
+        public string Komentet { get; set; }
+    }
+}
